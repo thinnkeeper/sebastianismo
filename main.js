85cbbd2855a7d2be9967c8a40a682d85965a8d1e
thinnkeeper <sebas94monteiro@gmail.com>
2025-02-17 15:35:22 +0000

    "rollback" remove create's

--- main.js
+++ main.js
@@ -43,24 +43,13 @@ function check_key(event){
     }
 }
 
-function create_help(){
-    const answer = 'Available commands:<br><span class="neon_green">about</span>&emsp;&emsp;&emsp;&emsp;section about me<br><span class="neon_green">academic</span>&emsp;&emsp;&emsp;&emsp;section about my academic path<br><span class="neon_green">projects</span>&emsp;&emsp;&emsp;&emsp;section about my personal projects<br><span class="neon_green">contacts</span>&emsp;&emsp;&emsp;&emsp;section about my contacts<br><span class="neon_green">clear || c</span>&emsp;&emsp;&emsp;&emsp;clear console';
-    return answer;
-}
-
 function add_help(){
     let box = document.querySelector(".box");
     let newElement = document.createElement("p");
     newElement.setAttribute("id", "help");
     box.appendChild(newElement);
-    document.getElementById("help").innerHTML = create_help();
-}
-
-function create_about(){
-    let answer = 'Hello, my name is <span class="neon_green">Sebastião Condeixa Sarreira Monteiro</span> I am a software/web developer from Portugal.<br>';
-    answer += 'I study computer science at Escola Superior de Tecnologia do Instituto Politécnico de Castelo Branco.<br>';
-    answer += 'As well as a programmer, I am also an avid reader and musician.<br>';
-    return answer;
+    let create_help = 'Available commands:<br><span class="neon_green">about</span>&emsp;&emsp;&emsp;&emsp;section about me<br><span class="neon_green">academic</span>&emsp;&emsp;&emsp;&emsp;section about my academic path<br><span class="neon_green">projects</span>&emsp;&emsp;&emsp;&emsp;section about my personal projects<br><span class="neon_green">contacts</span>&emsp;&emsp;&emsp;&emsp;section about my contacts<br><span class="neon_green">clear || c</span>&emsp;&emsp;&emsp;&emsp;clear console';
+    document.getElementById("help").innerHTML = create_help;
 }
 
 function add_about(){
@@ -68,12 +57,10 @@ function add_about(){
     let newElement = document.createElement("p");
     newElement.setAttribute("id", "about");
     box.appendChild(newElement);
-    document.getElementById("about").innerHTML = create_about();
-}
-
-function create_academic(){
-    let answer = '<span class="neon_green">Licenciatura em Informática e Multimédia</span>: 10/2024<br>Instituto Politécnico de Castelo Branco - Escola Superior de Tecnologia<br>http://www.ipcb.pt<br><span class="neon_green">Coursera Certificates</span>: 08/2021<br>Python for Everybody:<br>- Programming for Everybody (Getting Started with Python)<br>https://www.coursera.org/account/accomplishments/certificate/FJTRVLLP5DE2<br>- Python Data Structures<br>https://www.coursera.org/account/accomplishments/certificate/AXHGY65YLYXK<br>- Using Python to Access Web Data<br>https://www.coursera.org/account/accomplishments/certificate/2963C5CEBB6L<br>- Using Databases with Python<br>https://www.coursera.org/account/accomplishments/certificate/BA8D6942ULEB<br>- Capstone: Retrieving, Processing, and Visualizing Data with Python<br>https://www.coursera.org/account/accomplishments/certificate/Z6QJULXU295A<br>Django for Everybody:<br>- Web Application Technologies and Django<br>https://www.coursera.org/account/accomplishments/certificate/776CUELF8BZP<br><span class="neon_green">Nivél Secundário de Educação - Curso Científico-Humanístico de Artes Visuais</span>: 09/2010 - 07/2013<br>- Escola Secundária/3 de Amato Lusitano<br>http://aeal.edu.pt/<br>';
-    return answer;
+    let create_about = 'Hello, my name is <span class="neon_green">Sebastião Condeixa Sarreira Monteiro</span> I am a software/web developer from Portugal.<br>';
+    create_about += 'I study computer science at Escola Superior de Tecnologia do Instituto Politécnico de Castelo Branco.<br>';
+    create_about += 'As well as a programmer, I am also an avid reader and musician.<br>';
+    document.getElementById("about").innerHTML = create_about;
 }
 
 function add_academic(){
@@ -81,7 +68,8 @@ function add_academic(){
     let newElement = document.createElement("p");
     newElement.setAttribute("id", "academic");
     box.appendChild(newElement);
-    document.getElementById("academic").innerHTML = create_academic();
+    let create_academic = '<span class="neon_green">Licenciatura em Informática e Multimédia</span>: 10/2024<br>Instituto Politécnico de Castelo Branco - Escola Superior de Tecnologia<br>http://www.ipcb.pt<br><span class="neon_green">Coursera Certificates</span>: 08/2021<br>Python for Everybody:<br>- Programming for Everybody (Getting Started with Python)<br>https://www.coursera.org/account/accomplishments/certificate/FJTRVLLP5DE2<br>- Python Data Structures<br>https://www.coursera.org/account/accomplishments/certificate/AXHGY65YLYXK<br>- Using Python to Access Web Data<br>https://www.coursera.org/account/accomplishments/certificate/2963C5CEBB6L<br>- Using Databases with Python<br>https://www.coursera.org/account/accomplishments/certificate/BA8D6942ULEB<br>- Capstone: Retrieving, Processing, and Visualizing Data with Python<br>https://www.coursera.org/account/accomplishments/certificate/Z6QJULXU295A<br>Django for Everybody:<br>- Web Application Technologies and Django<br>https://www.coursera.org/account/accomplishments/certificate/776CUELF8BZP<br><span class="neon_green">Nivél Secundário de Educação - Curso Científico-Humanístico de Artes Visuais</span>: 09/2010 - 07/2013<br>- Escola Secundária/3 de Amato Lusitano<br>http://aeal.edu.pt/<br>';
+    document.getElementById("academic").innerHTML = create_academic;
 }
 
 function add_contacts(){
